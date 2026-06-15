1cfafe0796ad31aba9e763799d8185a3ac55db13
Diallo Fousseini <diallofousseini14@gmail.com>
2026-06-15 11:17:30 +0200

    Refactor computer mockup to use natural intrinsic sizing for flawless alignment

--- src/components/dronek/DsmPage.tsx
+++ src/components/dronek/DsmPage.tsx
@@ -350,15 +350,19 @@ export default function DsmPage({ onNavigate }: { onNavigate: (page: any) => voi
 
           <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 items-center">
             {/* Left Column: Computer Mockup Displaying Active Screen */}
-            <div className="lg:col-span-7 flex flex-col items-center">
-              <div className="relative w-full max-w-full sm:max-w-[600px] aspect-[653/382] transition-transform duration-500 mb-6">
+            <div className="lg:col-span-7 flex flex-col items-center justify-center">
+              <div className="relative w-full max-w-[600px] mx-auto mb-6">
+                {/* L'image de l'ordinateur qui dicte naturellement la taille du conteneur parent */}
                 <Image
                   src="/images/ordinateur.png"
-                  fill
-                  className="object-contain pointer-events-none z-10"
+                  width={653}
+                  height={382}
+                  className="w-full h-auto block pointer-events-none relative z-10"
                   alt="DSM Computer Mockup"
                   priority
                 />
+                
+                {/* L'écran interne dynamique, calé au pourcentage exact de l'ordinateur */}
                 <div className="absolute top-[6.28%] left-[10.26%] right-[10.72%] bottom-[4.45%] overflow-hidden bg-black z-20 rounded-sm">
                   <AnimatePresence mode="wait">
                     <motion.div
